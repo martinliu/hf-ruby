dac18b36ef824ac9a2caa4b3408b763fe5dbff77
martinliu <martin@aws-faq.com>
2015-12-03 01:45:02 +0800

    study ch01

---
+++ loop.rb
@@ -0,0 +1,11 @@
+number = 1
+while number <=5
+	puts number
+	number += 1
+end
+
+until number < 1
+	puts number
+	number -= 1
+	
+end
---
+++ test_code.rb
@@ -0,0 +1,21 @@
+puts "First Line\nSecond Line\nThird Line"
+puts "\tIntended line"
+puts "\"It's okay,\" he said."
+puts "One backslash: \\"
+puts 42.methods
+puts "hello".methods
+puts 42.class
+puts "hello".class
+puts true.class
+puts false.class
+puts rand(100)
+puts rand(100)
+score gets.to_i
+if score == 100
+	puts "Perfect!"
+elsif score >= 10
+	puts "You pass!"
+else
+	puts "Summer school time!"
+end
+
